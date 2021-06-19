1e47d6b1f1da7f92af3a7c8db04ad80c1b366ac8
salih <salihkoc3469@gmail.com>
2021-06-19 11:49:41 +0300

    Asp.Net Core API Unit Test-GetProduct() methodunun test edilmesi

---
+++ UdemyRealWorldUnitTest.Test/ProductApiTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyRealWorldUnitTest.Web.Controllers;
+using UdemyRealWorldUnitTest.Web.Models;
+using UdemyRealWorldUnitTest.Web.Repository;
+using Xunit;
+
+namespace UdemyRealWorldUnitTest.Test
+{
+    public class ProductApiTest
+    {
+        private readonly Mock<IRepository<Product>> _mockRepo;
+
+        private readonly ProductsApiController _controller;
+
+        private List<Product> products;
+        public ProductApiTest()
+        {
+            _mockRepo = new Mock<IRepository<Product>>();
+            _controller = new ProductsApiController(_mockRepo.Object);
+
+            products = new List<Product>() {
+               new Product { Id = 1, Name = "Kalem", Price = 100, Stock = 50, Color = "Kırmızı" },
+               new Product {Id=2,Name="Defter", Price=200, Stock=50, Color="Mavi" }
+            };
+        }
+
+        [Fact]
+        public async void GetProduct_ActionExecutes_ReturnOkResultWithProducts()
+        {
+            _mockRepo.Setup(x => x.GetAll()).ReturnsAsync(products);
+
+            var result =await _controller.GetProducts();
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+
+            var returnProduct = Assert.IsAssignableFrom<IEnumerable<Product>>(okResult.Value);
+
+            Assert.Equal<int>(2, returnProduct.ToList().Count);
+        }
+
+
+    }
+}
