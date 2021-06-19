57ba3ff5ed55efab6540ae0269cb2fb544b22560
salih <salihkoc3469@gmail.com>
2021-06-19 12:35:46 +0300

    Asp.Net Core API Unit Test - DeleteProduct methodunun test edilmesi-1

--- UdemyRealWorldUnitTest.Test/ProductApiTest.cs
+++ UdemyRealWorldUnitTest.Test/ProductApiTest.cs
@@ -120,5 +120,18 @@ namespace UdemyRealWorldUnitTest.Test
 
             Assert.Equal("GetProduct", createdAtActionResult.ActionName);
         }
+
+        [Theory]
+        [InlineData(0)]
+        public async void DeleteProduct_IdInValid_ReturnNotFound(int productId)
+        {
+            Product product = null;
+
+            _mockRepo.Setup(x => x.GetById(productId)).ReturnsAsync(product);
+
+            var resultNotFound = await _controller.DeleteProduct(productId);
+
+            Assert.IsType<NotFoundResult>(resultNotFound);
+        }
     }
 }
